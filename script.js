9ffb1ad7f370a5ee5613a49bc8e0e03870e71fc7
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-08 06:52:57 -0500

    Breakup fucntion into smaller functions

--- script.js
+++ script.js
@@ -8,107 +8,42 @@ let numberOkay = true;
 let continuingNumber = false;
 
 const screen = document.querySelector(".screen");
-
 const keysWrapper = document.querySelector(".keys-wrapper");
+
 keysWrapper.addEventListener("click", (event) => {
     const isButton = event.target.nodeName === "BUTTON";
     if (!isButton){
         return;
     }
     else if (event.target.id === "clear") {
-        console.log("hit clear!");
-        clear();
+        console.log("hit clear button!");
+        clearAll();
         return;
     }
     else if (event.target.id === "equals") { 
         console.log("hit equals!");
-        let number = parseFloat(screen.innerText); 
-        if (!isNaN(number)){
-            operands.push(number);
-        }
-        console.log("OPERANDS", operands, operators);
-
-        if (operators.length > 0) {
-            console.log("enough operators to evalaute!");
-            let calculation = evaluate();
-            console.log("calc = ", calculation);
-            if (isFinite(calculation)){
-                screen.innerText = `${truncate(calculation)}`;
-            }
-            else {
-                screen.innerText = "Nuh uh uh...";
-                operands.push(0);
-            }
-        }
-        enableButtons(".number", ".decimal", ".op"); 
-        continuingNumber = false;
-        console.log("after equals", operands, operators);
+        executeEquals();
     }
-
-    //BUG - when calculate a number (bby hitting operator or equals, currently allows you to add a decimal to result) 
-    else if (event.target.classList.contains("decimal")) {  
-        if (continuingNumber) {
-            console.log("OPERANDS", operands);
-            console.log("hit decimal!");
-            disableButtons(".decimal");
-            screen.innerText += ".";
-        }
+    else if (event.target.classList.contains("decimal")) {  //still bug, this one where 0. needs be added 
+        addDecimal();
     }
-
-    else if (event.target.classList.contains("binary")) { //got a binary op
+    else if (event.target.classList.contains("binary")) { 
         console.log("hit binary op!");
-        continuingNumber = false;
-        let number = parseFloat(screen.innerText); 
-        if (!isNaN(number)){
-            operands.push(number);
-        }
-        console.log("OPERANDS AFTER HITTING BINARY OP", operands, operators);
-
-        if (operators.length > 0) {
-            console.log("enough operators to evalaute!");
-            let calculation = evaluate();
-            console.log("calc = ", calculation);
-            if (isFinite(calculation)){
-                screen.innerText = `${truncate(calculation)}`;
-                operands.push(parseFloat(screen.innerText));
-            }
-            else {
-                screen.innerText = "Nuh uh uh...";
-                operands.push(0); //just reset
-            }
-        }
-        operators.push(event.target.id);
-        enableButtons(".decimal", ".number");
-        disableButtons(".binary");
+        addBinaryOperator(event.target.id);
     }
-    else if (event.target.classList.contains("number")) { //got a number
+    else if (event.target.classList.contains("number")) { 
         console.log("hit number!");
-        console.log("OPERANDS", operands);
-        if (!continuingNumber || screen.innerText === "0") {
-            screen.innerText = ""; 
-        }
-        screen.innerText += event.target.innerText; 
-        enableButtons(".op");
-        continuingNumber = true;
+        addNumber();
     }
-    else if (event.target.classList.contains("unary")) { //unary operator
-        console.log("OPERANDS", operands);
+
+    else if (event.target.classList.contains("unary")) { //unary operator - can this just be be?
         console.log("hit unary op!");
-        continuingNumber = false;
-        let number = parseFloat(screen.innerText);
-        if (!isNaN(number)){
-            number = evaluateUnary(event.target.id, number);
-            screen.innerText = `${number}`;
-            disableButtons(".number");
-        }
-        else {
-            operands.push(0);
-        }
+        executeUnary(event.target.id);
     }
     
 });
 
-function clear() {
+function clearAll() {
     wantOperator = false;
     numberOkay = true;
     operators = [];
@@ -116,6 +51,88 @@ function clear() {
 
     screen.innerText = "0"; 
     enableButtons(".number", ".decimal", ".op");
+}
+
+function executeEquals() {
+    let number = parseFloat(screen.innerText); //get whatever is on the screen
+        if (!isNaN(number)){
+            operands.push(number); //if it's not our error message, push it to the operand stack
+        }
+        console.log("OPERANDS", operands, operators);
+
+        if (operators.length > 0) { //check that we can perform a calculation
+            let calculation = evaluate();
+            console.log("CALC = ", calculation);
+
+            if (isFinite(calculation)){ 
+                screen.innerText = `${truncate(calculation)}`;
+            }
+            else {
+                screen.innerText = "Nuh uh uh..."; //warning for divide by zero
+                operands.push(0);
+            }
+        }
+        enableButtons(".number", ".decimal", ".op"); //enable any disabled buttons
+        continuingNumber = false;
+        console.log("after equals", operands, operators);
+}
+
+function addDecimal() {
+    if (continuingNumber) {
+        console.log("OPERANDS", operands);
+        console.log("hit decimal!");
+        disableButtons(".decimal");
+        screen.innerText += ".";
+    }
+}
+
+function addBinaryOperator(id) {
+    continuingNumber = false;
+    let number = parseFloat(screen.innerText); 
+    if (!isNaN(number)){
+        operands.push(number);
+    }
+        console.log("OPERANDS AFTER HITTING BINARY OP", operands, operators);
+
+    if (operators.length > 0) {
+        console.log("enough operators to evalaute!");
+        let calculation = evaluate();
+        console.log("calc = ", calculation);
+        if (isFinite(calculation)){
+            screen.innerText = `${truncate(calculation)}`;
+            operands.push(parseFloat(screen.innerText));
+        }
+        else {
+            screen.innerText = "Nuh uh uh...";
+            operands.push(0); //just reset
+        }
+    }
+    operators.push(id);
+    enableButtons(".decimal", ".number");
+    disableButtons(".binary");
+}
+
+function addNumber() {
+    console.log("OPERANDS", operands);
+    if (!continuingNumber || screen.innerText === "0") {
+        screen.innerText = ""; 
+    }
+    screen.innerText += event.target.innerText; 
+    enableButtons(".op");
+    continuingNumber = true;
+}
+
+function executeUnary(id) {
+    continuingNumber = false;
+    let number = parseFloat(screen.innerText);
+    if (!isNaN(number)){
+        number = evaluateUnary(id, number);
+        screen.innerText = `${number}`;
+        disableButtons(".number");
+    }
+    else {
+        operands.push(0);
+    }
 }
 
 //controlling the input by disabling, enabling buttons 
@@ -138,13 +155,14 @@ function enableButtons() {
 }
 
 function evaluate() {
-    console.log("evaluating....");
-    console.log("operators", operators);
-    console.log("operands", operands);
+
+    console.log("evaluting with operands", operands, "and operators", operators);
     const op = operators.pop();
     const x = operands.pop();
     const y = operands.pop(); 
+
     console.log("x, y", x, y, op);
+
     let result; 
     if (op === "add"){
         return y + x;
